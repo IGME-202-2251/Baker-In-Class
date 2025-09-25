c1132550a23fdc54657eea106583f4cf6d303efd
Eric Baker <8997206+patches34@users.noreply.github.com>
2025-09-25 10:34:18 -0400

    move object with physics

--- 2D Project/Assets/Scripts/PlayerMovement.cs
+++ 2D Project/Assets/Scripts/PlayerMovement.cs
@@ -12,24 +12,49 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Vector3 velocity;
 
+    [SerializeField]
+    Rigidbody2D rbody2D;
+
+    public bool usePhysics = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //rbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //  calc velocity
-        velocity = movementDirection * speed * Time.deltaTime;
+        if (!usePhysics)
+        {
+            //  calc velocity
+            velocity = movementDirection * speed * Time.deltaTime;
 
-        //  move GameObject
-        transform.position += velocity;
+            //  move GameObject
+            transform.position += velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (usePhysics)
+        {
+            //  calc velocity
+            velocity = movementDirection * speed * Time.fixedDeltaTime;
+
+            //  move GameObject
+            rbody2D.MovePosition(transform.position + velocity);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movementDirection = context.ReadValue<Vector2>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
     }
 }
