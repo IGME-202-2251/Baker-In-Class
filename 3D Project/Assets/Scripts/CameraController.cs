c24aa3cf6c0eef602dc14eadc78f0900d1a0d9c9
Eric Baker <8997206+patches34@users.noreply.github.com>
2025-11-11 10:46:09 -0500

    Rotation demo

---
+++ 3D Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Camera playerCamera;
+
+    [SerializeField]
+    Vector2 mouseInput;
+
+    [SerializeField]
+    Vector2 lookSensitifity = Vector2.one;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTurn(InputAction.CallbackContext context)
+    {
+        mouseInput = context.ReadValue<Vector2>();
+
+        //  Turn left/right
+        transform.rotation = transform.rotation * Quaternion.Euler(0f, mouseInput.x * lookSensitifity.x, 0f);
+
+        //  Look up/down
+        playerCamera.transform.Rotate(mouseInput.y * lookSensitifity.y, 0f, 0f);
+    }
+}
