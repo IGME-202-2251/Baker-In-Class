63c8bff34d91e1f1d636074ddc6cb438d8a906ee
Eric Baker <8997206+patches34@users.noreply.github.com>
2025-11-13 10:15:26 -0500

    raycasting demo

---
+++ 3D Project/Assets/Scripts/ObjectCLcker.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectCLcker : MonoBehaviour
+{
+    RaycastHit hitInfo;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.gameObject.name);
+            }
+        }
+    }
+}
