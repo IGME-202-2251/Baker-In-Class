e031570bbee6f83f811f952e7927476d4e9d1a34
Eric Baker <8997206+patches34@users.noreply.github.com>
2025-11-20 10:30:33 -0500

    Ground alignment working!!!!!!!!!!!!

--- 3D Project/Assets/Scripts/GroundSnappingController.cs
+++ 3D Project/Assets/Scripts/GroundSnappingController.cs
@@ -15,11 +15,16 @@ public class GroundSnappingController : MonoBehaviour
     {
         if(Physics.Raycast(transform.position, -transform.up, out hitInfo, Mathf.Infinity, goundLayerMask))
         {
-            Vector3 snapPoint = hitInfo.point;
-            snapPoint.y += yOffset;
+            Vector3 snapPoint = transform.position;
+            snapPoint.y = hitInfo.point.y + yOffset;
 
             //  Snap to ground
             transform.position = snapPoint;
+
+            //  Align with ground
+            Vector3 newForward = Vector3.Cross(transform.right, hitInfo.normal);
+
+            transform.LookAt(transform.position + newForward, hitInfo.normal);
         }
     }
 }
